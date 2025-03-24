3c51df1beab13449c300b658a86d8d4a9f9afb82
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2025-03-24 01:09:26 +0000

    update

--- client/src/component/videos/Videos.js
+++ client/src/component/videos/Videos.js
@@ -48,7 +48,11 @@ const Videos = () => {
                 ></iframe>
               </div>
               <div className="card-body d-flex flex-column">
-                <h5 className="card-title video-title" title={video.title}>{video.title}</h5>
+                <div style={{ maxWidth: '250px' }}>
+                  <h5 className="card-title video-title" title={video.title}>
+                      {video.title}
+                  </h5>
+                </div>
                 <div className="flex column">{<LikeComponent />}</div>
               </div>
 
